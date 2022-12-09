3e456f87ee9c549f85a7ef41203ae94839c66f17
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-12-09 18:26:21 -0300

    Adição do select do estado e do município

--- src/Page/Principal.js
+++ src/Page/Principal.js
@@ -23,7 +23,7 @@ export const  Principal = () => {
     const ThirdPart = {
         margin: '3% 20% 3% 20%',
         backgroundColor: "white",
-        height: "40%",
+        height: "45%",
         width: "66%",
         borderRadius: "0.4rem",
         display: "flex",
@@ -40,7 +40,6 @@ export const  Principal = () => {
         fontWeight: "bold"
     }
     const textConsulta = {
-        marginBottom: '4%',
         width: '95%',
         display: "flex",
         alignItems: "center",
@@ -49,19 +48,71 @@ export const  Principal = () => {
         fontSize: '120%',
         color: "#474747"
     }
+    const titleEstadoMunicipio = {
+        height: "30%",
+        fontSize: '120%',
+        color: "#474747",
+    }
+    const selectEstadoMunicipio = {
+        height: "50%",
+        width: "100%"
+    }
+    const buttonConsultar = {
+        height: "50%",
+        display: "flex",
+        paddingRight: "2rem",
+        justifyContent: "flex-end",
+        paddingBottom: "1rem",
+    }
+    const button = {
+        width: "6rem",
+        height: "2.7rem",
+        backgroundImage: "linear-gradient(90deg, #052000 20%, #0D5200 80%)",
+        borderRadius: "0.2rem",
+        border: 0,
+        color: "white",
+    }
 
   return (
     <Grid  container style={containerStyle}>
         <Grid item sm={12} style={titlePrincipal}>
             IPAM
         </Grid>
-        <Grid item sm={12} spacing={2}style={SecondPart}>
+        <Grid item sm={12} style={SecondPart}>
             <Grid item style={ThirdPart}>
                 <Typography style={titleConsulta}>Consulta de dados</Typography>
                 <Typography style={textConsulta}>
                     Caso queira consultar dados relativos a um município, selecione 
                     um estado e um município abaixo.
                 </Typography>
+                <Grid 
+                    container
+                    item
+                    direction="column"
+                    justifyContent="center"
+                    style={{marginLeft: "0.8rem"}}>
+                    <Grid style={{width: '45%'}}>
+                        <Typography style={titleEstadoMunicipio}>Estado</Typography>
+                        <select name="select" style={selectEstadoMunicipio}>
+                            <option></option>
+                            <option value="valor1">Valor 1</option>
+                            <option value="valor2">Valor 2</option>
+                            <option value="valor3">Valor 3</option>
+                        </select>
+                    </Grid>
+                    <Grid style={{width: '45%'}}>
+                        <Typography style={titleEstadoMunicipio}>Município</Typography>
+                        <select name="select" style={selectEstadoMunicipio}>
+                            <option></option>
+                            <option value="valor1">Valor 1</option>
+                            <option value="valor2">Valor 2</option>
+                            <option value="valor3">Valor 3</option>
+                        </select>
+                    </Grid>
+                </Grid>
+                <Grid item style={buttonConsultar}>
+                    <button style={button}>Consultar</button>
+                </Grid>
             </Grid>
         </Grid>
     </Grid>
